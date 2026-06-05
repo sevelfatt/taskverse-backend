23c02b6838abca5b19223249f48a65e9980691a5
Sevalino Elfata <elfataweb@example.com>
2026-06-05 09:27:01 +0700

    feat: define task domain models including base Task and specific task types

--- models/task.go
+++ models/task.go
@@ -3,19 +3,31 @@ package models
 import "time"
 
 type Task struct {
-	UUID        string    `json:"uuid"`
-	Title       string    `json:"title"`
-	Type        string    `json:"type"`
-	DaysInWeek  []string  `json:"days_in_week"`
-	OneTimeTask bool      `json:"one_time_task"`
-	StartDate   time.Time `json:"start_date"`
-	EndDate     time.Time `json:"end_date"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	UUID string `json:"uuid"`
+	Title string `json:"title"`
+	Type string `json:"type"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
-type CompletedTask struct {
-	UUID        string    `json:"uuid"`
-	TaskUUID    string    `json:"task_uuid"`
-	CompletedAt time.Time `json:"completed_at"`
+type HabitTask struct {
+	Task
+	DaysInWeek []string `json:"days_in_week"`
+}
+
+type WeeklyTask struct {
+	Task
+	StartDate time.Time `json:"start_date"`
+	EndDate   time.Time `json:"end_date"`
+}
+
+type MonthlyTask struct {
+	Task
+	StartDate time.Time `json:"start_date"`
+	EndDate   time.Time `json:"end_date"`
+}
+
+type OneTimeTask struct {
+	Task
+	Date time.Time `json:"date"`
 }
